89cf7ceeb123f644330999aef205dcad0446be36
thanosk97 <thanosk97@gmail.com>
2024-11-06 17:37:44 +0200

    Basic inital website with redirecting urls

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -20,13 +20,17 @@ monthly_challenges = {
 
 # Create your views here.
 
-def monthly_challenge(request, month):
-    try:
-        challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
-    except:
-        return HttpResponseNotFound("This month is not supported yet!")
+def index(requests):
+    list_items = ""
+    months = list(monthly_challenges.keys())
 
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(list_items)
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
@@ -35,3 +39,11 @@ def monthly_challenge_by_number(request, month):
     redirect_month = months[month-1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("<h1>This month is not supported yet!</h1>")
