b98ebbd586f37672610f9a244cc53250c7761027
thanosk97 <thanosk97@gmail.com>
2024-11-10 18:59:55 +0200

    templates

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -20,17 +20,12 @@ monthly_challenges = {
 
 # Create your views here.
 
-def index(requests):
-    list_items = ""
+def index(request):
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(list_items)
+    return render(request, "challenges/index.html", {
+        "months": months
+    }) 
 
 
 def monthly_challenge_by_number(request, month):
